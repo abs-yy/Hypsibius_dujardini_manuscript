d665d8d0d4cb6c568da461afb635d9e778aef931
Yuki Yoshida <abs@sfc.keio.ac.jp>
2017-01-26 23:58:57 +0900

    Create screen_complete_proteome_from_uniprot_division.pl

---
+++ keio_scripts/screen_complete_proteome_from_uniprot_division.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use G;
+
+# perl screen_complete_proteome_from_uniprot_division.pl EBML_format.dat
+## EMBL_format.dat ex : uniprot_sprot_archaea.dat
+
+my $input = shift;
+my %out = &get_fasta($input);
+
+say to_fasta(%out);
+
+
+sub get_fasta{
+  my $input = $_[0];
+  my $tree = readFile($input, -format=>"swiss" );
+  my ($dat, $div) = (split /\_/, $input)[1,2];
+
+  $div =~ s/.dat//;
+
+  foreach my $entry ( sort keys %{$tree} ) {
+    if( defined $tree->{$entry}->{KW} && $tree->{$entry}->{KW} =~ /Complete\sproteome/ ) {
+      my %fasta;
+      my $seq = $tree->{$entry}->{"  "};
+      $seq =~ s/\s+//g;
+      say $tree->{$entry}->{LOCUS}->{id}."|".$dat."|".$div if $tree->{$entry}->{OC} =~ /Metazoa/;
+      $fasta{$tree->{$entry}->{LOCUS}->{id}."|".$dat."|".$div} = $seq;
+    }
+  }
+  return %fasta;
+}
